cc22e9fa4ea3a612728ab62d57a25fc2dc65c746
Max Heikenfeld <max.heikenfeld@deutschebahn.com>
2026-06-23 17:40:44 +0200

    load sbb as oebb for now

--- src/dreamteam/scripts/run_combine_data.py
+++ src/dreamteam/scripts/run_combine_data.py
@@ -1,13 +1,13 @@
 from dreamteam.data_handling.combine_data import combine_data
 from pathlib import Path
 
-path_ares = Path("sample_data/ARES.csv")
-path_fahrplan_zuege_sbb = Path("sample_data/Fahrplan_zuege.csv")
-path_fahrplan_zuege_oebb = Path("sample_data/Fahrplan_zuege.csv")
-path_fahrplan_zuege_db = Path("sample_data/Fahrplan_zuege.csv")
-path_fahrplan_zueglaeufe_sbb = Path("sample_data/Fahrplan_zuglaeufe.csv")
-path_fahrplan_zueglaeufe_oebb = Path("sample_data/Fahrplan_zuglaeufe.csv")
-path_fahrplan_zueglaeufe_db = Path("sample_data/Fahrplan_zuglaeufe.csv")
+path_ares = Path("data/ares_cleaned.csv")
+path_fahrplan_zuege_sbb = Path("data/sbb_data.csv")
+path_fahrplan_zuege_oebb = Path("data/sbb_data.csv")
+path_fahrplan_zuege_db = Path("data/db_data.csv")
+path_fahrplan_zueglaeufe_sbb = Path("data/sbb_data.csv")
+path_fahrplan_zueglaeufe_oebb = Path("data/sbb_data.csv")
+path_fahrplan_zueglaeufe_db = Path("data/db_data.csv")
 path_output_zuege = Path("data/output_zuege.csv")
 path_output_zueglaeufe = Path("data/output_zueglaeufe.csv")
 
