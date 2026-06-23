f5b22dfb83f02f753c40afbbe940dc7142f45010
Maelle Wannier <maelle.wannier@sbb.ch>
2026-06-23 17:41:02 +0200

    clean zugnummer

--- src/dreamteam/data_handling/ares_cleaning.py
+++ src/dreamteam/data_handling/ares_cleaning.py
@@ -5,6 +5,9 @@ path = 'src/dreamteam/data/ares_H4R.xlsx'
 def clean_ares_data(path:str) -> None:
     data = pd.read_excel(path, skiprows=2)
 
+    #clean zugnummer from text
+    data["Zug"] = data["Zug"].str.extract(r'(\d+)').astype(int)
+
     #retrieve start and end date from the column "Datum ab - bis" and convert to datetime
     data["start_date"]= pd.to_datetime(data['Datum ab - bis'].str.split(' - ').str[0], format='%d.%m.%Y', errors='coerce')
     data["end_date"]= pd.to_datetime(data['Datum ab - bis'].str.split(' - ').str[1].fillna(data["start_date"]), format='%d.%m.%Y', errors='coerce')
